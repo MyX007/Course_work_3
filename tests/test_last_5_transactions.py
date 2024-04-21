7d5dce633c5a7b4794419216b4c3435cc87b9b33
Vladimir Blyntsev <blyncev@yandex.ru>
2024-04-21 22:27:16 +0700

    Add comments in tests, and remove import 'from funcs.last_5_transactions import get_operations_list' in test_format_transactions.py

--- tests/test_last_5_transactions.py
+++ tests/test_last_5_transactions.py
@@ -1,8 +1,14 @@
 from funcs import last_5_transactions
 
 def test_lenght_transactions():
+    """
+    Проверяет функкцию, чтобы длина списка была равна 5
+    """
     assert len(last_5_transactions.get_operations_list()) == 5
 
 def  test_executed_status():
+    """
+    Проверяет наличие статуса "EXECUTED" у всех операций из списка
+    """
     for i in last_5_transactions.get_operations_list():
         assert i["state"] == "EXECUTED"
